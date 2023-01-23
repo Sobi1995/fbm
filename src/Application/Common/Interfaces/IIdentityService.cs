103b4789cef7c6f2ba0921540c0842f52a971d22
sobhan mozafari <Sobhan.Plus.Plus@gmail.com>
2023-01-23 16:47:58 +0330

    init

---
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Application.Common.Models;
+
+namespace CleanArchitecture.Application.Common.Interfaces;
+
+public interface IIdentityService
+{
+    Task<string?> GetUserNameAsync(Guid userId);
+
+    Task<bool> IsInRoleAsync(Guid userId, string role);
+
+    Task<bool> AuthorizeAsync(Guid userId, string policyName);
+
+    Task<(Result Result, Guid UserId)> CreateUserAsync(string userName, string password);
+
+    Task<Result> DeleteUserAsync(Guid userId);
+}
---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using System.Reflection;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Infrastructure.Identity;
+using CleanArchitecture.Infrastructure.Persistence.Interceptors;
+using MediatR;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CleanArchitecture.Infrastructure.Persistence;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Role, Guid>, IApplicationDbContext
+{
+
+
+
+    private readonly IMediator _mediator;
+    private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+
+    public ApplicationDbContext(
+        IMediator mediator,
+        AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor,
+        DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+    {
+        _mediator = mediator;
+        _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+    }
+    public DbSet<TodoList> TodoLists => Set<TodoList>();
+
+    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await _mediator.DispatchDomainEvents(this);
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
+
+
